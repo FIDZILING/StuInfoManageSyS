82eb171b305128349e7d3f581e0c5781e78c9685
孙镇江 林 <312865797@qq.com>
2017-12-30 20:16:08 +0800

    傻玲玲

---
+++ StuInfoMaSys/BLL/DealBLL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+    public class DealBLL
+    {
+        private DealDAL dealDAL = new DealDAL();
+        /// <summary>
+        /// 判断输入的用户名和密码是否匹配，匹配返回1
+        /// </summary>
+        /// <param name="User_Name">用户名</param>
+        /// <param name="User_Pwd">密码</param>
+        /// <returns></returns>
+        public int Log_In(string User_Name, string User_Pwd)
+        {
+            return dealDAL.SignIn_User(User_Name, User_Pwd);
+        }
+
+        public void Power_Judge(DataTable Data_Table)
+        {
+
+        }
+    }
+}
---
+++ StuInfoMaSys/DAL/DealDAL.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DealDAL
+    {
+        private static string Sql_Con_Str = "server=localhost;database=StuInfoDB;user id=sa;pwd=sa";
+        /// <summary>
+        /// 判断输入的用户名和密码是否匹配，匹配返回1
+        /// </summary>
+        /// <param name="User_Name">用户名</param>
+        /// <param name="User_Pwd">密码</param>
+        /// <returns></returns>
+        public int SignIn_User(string User_Name, string User_Pwd)
+        {
+            StringBuilder Sql_Str = new StringBuilder();
+            Sql_Str.Append("select * from dbo.Users");
+            Sql_Str.Append(" where Name=@Name and Password=@Password");
+            SqlParameter[] Paras =
+                {
+                    new SqlParameter("@Name",User_Name),
+                    new SqlParameter("@Password",User_Pwd)
+                };
+            SqlConnection Conn = new SqlConnection(Sql_Con_Str);
+            try
+            {
+                Conn.Open();
+                SqlCommand Cmd = new SqlCommand(Sql_Str.ToString(), Conn);
+                DataTable Data_Table = new DataTable();
+                SqlDataReader dr = Cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                Data_Table.Load(dr);
+
+                Cmd.Parameters.AddRange(Paras);
+                return (int)Cmd.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+            finally
+            {
+                if (Conn != null)
+                {
+                    Conn.Dispose();
+                }
+            }
+        }
+
+
+    }
+}
